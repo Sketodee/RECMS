5052418d200ca5ab7a3cf64eb14afe159ad2d6cb
Shobande James Oluwaseun <shobandejames@gmail.com>
2023-01-25 16:06:04 +0100

    FE - fixed navbar collapse on clicking link

--- RECMS/ClientApp/src/components/Layout.js
+++ RECMS/ClientApp/src/components/Layout.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Outlet } from 'react-router-dom'
 import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
-import MyNav from './MyNav'
+import MyNav from "./MyNav"
 
 const Layout = () => {
     return (
         <>
-            <Container>
-                <Outlet />
-            </Container>
+        <Container>
+            <MyNav />
+            <Outlet />
+        </Container>
         </>
         
     )
--- RECMS/ClientApp/src/components/MyNav.js
+++ RECMS/ClientApp/src/components/MyNav.js
@@ -1,56 +1,78 @@
-﻿import React, { useState } from 'react';
-import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-    NavbarText,
-} from 'reactstrap';
+﻿import react, { useState } from 'react'
+import {Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import useAuth from '../hooks/useAuth'
 
-function MyNav(args) {
-    const [isOpen, setIsOpen] = useState(false);
+const MyNav = () => {
 
-    const toggle = () => setIsOpen(!isOpen);
+    const { auth } = useAuth()
 
     return (
-        <div>
-            <Navbar {...args}>
-                <NavbarBrand href="/">reactstrap</NavbarBrand>
-                <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
-                    <Nav className="me-auto" navbar>
-                        <NavItem>
-                            <NavLink href="/components/">Components</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="https://github.com/reactstrap/reactstrap">
-                                GitHub
-                            </NavLink>
-                        </NavItem>
-                        <UncontrolledDropdown nav inNavbar>
-                            <DropdownToggle nav caret>
-                                Options
-                            </DropdownToggle>
-                            <DropdownMenu right>
-                                <DropdownItem>Option 1</DropdownItem>
-                                <DropdownItem>Option 2</DropdownItem>
-                                <DropdownItem divider />
-                                <DropdownItem>Reset</DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                    </Nav>
-                    <NavbarText>Simple Text</NavbarText>
-                </Collapse>
-            </Navbar>
-        </div>
-    );
+        <>
+            {['sm'].map((expand) => (
+                <Navbar key={expand} bg="light" collapseOnSelect expand={expand} className="mb-3">
+                    <Container fluid>
+                        <Navbar.Brand href="#"> <h2> RECMS </h2> </Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Item> <Nav.Link eventKey="1" as={Link} to="/"> Home</Nav.Link> </Nav.Item>
+                                    <Nav.Item> <Nav.Link eventKey="1" as={Link} to="/counter"> Counter </Nav.Link> </Nav.Item>
+                                    {/*<Nav.Link as={Link} to="/"> Home</Nav.Link>*/}
+                                    {/*<Nav.Link as={Link} to= "/counter"> Counter </Nav.Link>*/}
+                                    <NavDropdown
+                                        title="Dropdown"
+                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                                    >
+                                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action4">
+                                            Another action
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action5">
+                                            Something else here
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+
+                                {auth?.email ?
+                                    <div class="dropdown">
+                                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" />
+                                            <strong className="text-dark"> mdo </strong>
+                                        </a>
+                                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                                            <li><hr class="dropdown-divider" /></li>
+                                            <li><a class="dropdown-item" href="#">Sign out</a></li>
+                                        </ul>
+                                    </div>
+                                    : null}
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+    )
 }
 
-export default MyNav;
+export default MyNav
